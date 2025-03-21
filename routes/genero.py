af857d4ce8292902aa4fec2cbf11bbbbd109c043
DarwinGmez <firemarlon2311@gmail.com>
2025-03-21 17:33:44 -0500

    aGREGAR

--- routes/genero.py
+++ routes/genero.py
@@ -2,13 +2,15 @@ from flask import request
 from app import app,db
 from models.genero import *
 from sqlalchemy import exc
+from flask import session
+from app import db
 
 
 @app.route("/genero/",methods=['GET'])
 def listarGeneros():
     try:
         mensaje=None
-        generos=Genero.query.all()
+        generos=Genero.query.order_by(Genero.idGenero).all()
         listarGeneros=[]
         for g in generos:
             genero={
@@ -20,3 +22,25 @@ def listarGeneros():
         mensaje=str(error)
     
     return {"mensaje":mensaje,"generos":listarGeneros}
+
+
+
+
+@app.route("/genero/",methods=['POST'])
+def addGenero():
+    try:
+        mensaje=None
+        estado=False
+        if request.method=='POST':
+            datos=request.get_json()
+            
+            genero=Genero(genNombre=datos['genero'])
+            db.session.add(genero)
+            db.session.commit()
+            estado=True
+            mensaje="Genero agregado correctamente"
+        else:
+            mensaje="Tarea no permitida"
+    except exc.SQLAlchemyError as error:
+        db.session.rollback()
+    return {"estado":estado,"mensaje":mensaje}
--- routes/peliculas.py
+++ routes/peliculas.py
@@ -2,6 +2,7 @@ from flask import request
 from app import app,db
 from models.genero import *
 from models.pelicula import *
+from models.genero import *
 from sqlalchemy import exc
 
 
@@ -21,7 +22,7 @@ def listarPeliculas():
                     "duracion":p.pelDuracion,
                     "genero":{
                         "idGenero":p.genero.idGenero,
-                        "nombre":p.genero.GenNombre
+                        "nombre":p.genero.genNombre
                     },
                     "foto":p.pelFoto
                 }
@@ -34,3 +35,32 @@ def listarPeliculas():
         mensaje=str(error)
     
     return {"mensaje":mensaje,"Peliculas":listarPeliculas}
+
+
+
+@app.route("/pelicula/",methods=['POST'])
+def addPelicula():
+    try:
+        mensaje=None
+        estado=False
+        if request.method=='POST':
+            datos=request.get_json()
+            genero=Genero.query.get(int(datos['genero']))
+            pelicula=Pelicula(
+                pelCodigo=datos['codigo'],
+                pelTitulo=datos['titulo'],
+                pelProtagonista=datos['protagonista'],
+                pelDuracion=datos['duracion'],
+                pelResumen=datos['resumen'],
+                pelFoto=datos['foto'],
+                # pelGenero=datos['genero']
+                genero=genero)
+            db.session.add(pelicula)
+            db.session.commit()
+            estado=True
+            mensaje="Pelicula agregado correctamente"
+        else:
+            mensaje="Tarea no permitida"
+    except exc.SQLAlchemyError as error:
+        db.session.rollback()
+    return {"estado":estado,"mensaje":mensaje}
